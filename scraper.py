db1def2b41d39a2bb2ad80b45f8f8346b7877a3b
cs-fedy <fedi.abd01@gmail.com>
2020-09-24 19:00:37 +0100

    refactoring code done 🥳

--- scraper.py
+++ scraper.py
@@ -4,39 +4,81 @@ from time import sleep
 import json
 
 # TODO: use multiple threads to scrape data from posts.
-# TODO: scroll to the buttom of the page and get all post url.
 # TODO: allow user to login to his account to explore private accounts
-# TODO: make a class that scrape post data
 
 
-class InstagramAccountScraper:
-    def __init__(self, account_url):
-        self.account_url = account_url
-        self.browser = webdriver.Chrome()
+class BasePage:
+    def load_full_page_source_code(self, callback=None):
+        if callback:
+            callback()
+        return self.browser.page_source
 
-    def __wait_until_page_is_loaded(self, page_url=None):
-        if not page_url:
-            page_url = self.account_url
+    def wait_until_page_is_loaded(self, page_url):
         self.browser.get(page_url)
         sleep(10)
         print(f"@@@ {page_url} is loaded @@@")
 
-    def __load_full_page_source_code(self):
-        # * scroll to buttom of the webpage
-        # scrolling_script = "window.scrollTo(0,document.body.scrollHeight)"
-        # content_page = ""
-        # while content_page != self.browser.page_source:
-        #     content_page = self.browser.page_source
-        #     self.browser.execute_script(scrolling_script)
-        #     try:
-        #         see_more = self.browser.find_element_by_xpath("//*[contains(text(), 'Show More Posts from')]")
-        #         see_more.click()
-        #     except:
-        #         pass
 
-        #     sleep(5)
+class InstagramPostScraper(BasePage):
+    def __init__(self, post_url, browser):
+        self.post_url = post_url
+        BasePage.__init__(self)
+        if not browser:
+            self.browser = webdriver.Chrome()
+        else:
+            self.browser = browser
+
+    def get_post_details(self):
+        self.wait_until_page_is_loaded(self.post_url)
+        source_code = self.load_full_page_source_code()
+        soup = BeautifulSoup(source_code, 'html.parser')
+        article = soup.find("article")
+        # * get post picture
+        pictures = article.find_all("img")[1]
+        post_pict = pictures["src"]
+
+        # * get post description
+        description_element = soup.find("h2").find_next_sibling()
+        description = description_element.getText()
+
+        # * get likes count
+        likes_section = article.select("section")
+        likes = likes_section[1].getText()
+
+        # * get share date
+        date_element = article.find_all("time")[-1]
+        date = date_element["datetime"]
+
+        return {
+            "url": self.post_url,
+            "picture_url": post_pict,
+            "description": description,
+            "likes_count": likes,
+            "share_date": date
+        }
+
+
+class InstagramAccountScraper(BasePage):
+    def __init__(self, account_url, browser):
+        self.account_url = account_url
+        self.browser = browser
+        BasePage.__init__(self)
+
+    def __scroll_to_the_buttom(self):
+        # * scroll to buttom of the webpage
+        scrolling_script = "window.scrollTo(0,document.body.scrollHeight)"
+        content_page = ""
+        while content_page != self.browser.page_source:
+            content_page = self.browser.page_source
+            self.browser.execute_script(scrolling_script)
+            try:
+                see_more = self.browser.find_element_by_xpath(
+                    "//*[contains(text(), 'Show More Posts from')]")
+                see_more.click()
+            except:
+                pass
+            sleep(5)
         print("@@@ page is fully loaded @@@")
-        return self.browser.page_source
 
     def __get_profile_details(self, source_code):
         soup = BeautifulSoup(source_code, "html.parser")
@@ -68,7 +110,7 @@ class InstagramAccountScraper:
             account_status = account_status_element.getText()
         else:
             account_status = "not verified"
-        
+
         # * get account bio
         bio = details_element.find("span").getText()
 
@@ -90,36 +132,11 @@ class InstagramAccountScraper:
 
     def __get_posts_details(self, source_code):
         urls = self.__get_posts_links(source_code)
-        return [self.__get_post_details(url) for url in urls]
-
-    def __get_post_details(self, post_url):
-        self.__wait_until_page_is_loaded(post_url)
-        source_code = self.__load_full_page_source_code()
-        soup = BeautifulSoup(source_code, 'html.parser')
-        article = soup.find("article")
-        # * get post picture
-        pictures = article.find_all("img")[1]
-        post_pict = pictures["src"]
-
-        # * get post description
-        description_element = soup.find("h2").find_next_sibling()
-        description = description_element.getText()
-
-        # * get likes count
-        likes_section = article.select("section")
-        likes = likes_section[1].getText()
-
-        # * get share date
-        date_element = article.find_all("time")[-1]
-        date = date_element["datetime"]
-
-        return {
-            "url": post_url,
-            "picture_url": post_pict,
-            "description": description,
-            "likes_count": likes,
-            "share_date": date
-        }
+        posts_details = []
+        for url in urls:
+            post_scraper = InstagramPostScraper(url, self.browser)
+            posts_details.append(post_scraper.get_post_details())
+        return posts_details
 
     def __form_final_data(self, profile_details, posts_details):
         data = {
@@ -130,14 +147,23 @@ class InstagramAccountScraper:
             json.dump(data, json_file)
 
     def __call__(self):
-        self.__wait_until_page_is_loaded()
-        source_code = self.__load_full_page_source_code()
+        self.wait_until_page_is_loaded(self.account_url)
+        source_code = self.load_full_page_source_code()
         account_details = self.__get_profile_details(source_code)
         posts_details = self.__get_posts_details(source_code)
         self.__form_final_data(account_details, posts_details)
 
 
+def create_headless_browser():
+    options = Options()
+    options.set_headless()
+    assert options.headless  # assert Operating in headless mode
+    driver = webdriver.Chrome(options=options)
+    return webdriver.Chrome()
+
+
 if __name__ == "__main__":
+    browser = create_headless_browser()
     url = "https://www.instagram.com/medium/?hl=en"
-    instagram_scraper = InstagramAccountScraper(url)
+    instagram_scraper = InstagramAccountScraper(url, browser)
     instagram_scraper()
